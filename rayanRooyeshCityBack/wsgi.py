10b86cc39cb5289cfd69922c692b9d87675642cc
MSNP1381 <mohamadnematpoor@gmail.com>
2022-03-23 12:54:07 +0430

    deploy to pythonanywhere

--- rayanRooyeshCityBack/wsgi.py
+++ rayanRooyeshCityBack/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for rayanRooyeshCityBack project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
+# +++++++++++ DJANGO +++++++++++
+# To use your own Django app use code like this:
 import os
+import sys
 
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/msnp1/mysite'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rayanRooyeshCityBack.settings')
-
 application = get_wsgi_application()
+###### or, for older Django <=1.4
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
